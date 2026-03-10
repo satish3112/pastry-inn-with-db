37acee9e251aca89f3fe8ce978a478f498b9b361
satish3112 <satishdadisetti31@gmail.com>
2026-03-10 11:48:10 +0530

    addedWishlist

--- src/components/MenuCard.js
+++ src/components/MenuCard.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState} from "react";
 import { toggleWishlist, isWishlisted } from "../firebase";
 
 export default function MenuCard({ item, cartItem, addToCart, updateQty }) {
